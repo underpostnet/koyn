68c6e1d50421a4cd1f9766b5ccda4e53516ed6a8
fcoverdugoa <fcoverdugoa@underpost.net>
2021-06-11 10:04:15 -0400

    update ws

--- src/ws.scm
+++ src/ws.scm
@@ -7,17 +7,29 @@
 (require net/rfc6455)
 (ws-serve* #:port 8081
            (ws-service-mapper
-            ["/test" ; the URL path (regular expression)
+            ["/path" ; the URL path (regular expression)
              [(subprotocol) ; if client requests subprotocol "subprotocol"
-              (lambda (c) (ws-send! c "You requested a subprotocol"))]
+              (lambda (c)
+
+                (ws-send! c "You requested a subprotocol")
+                (display "ws server protocol msg received \n")
+
+
+              )]
              [(#f) ; if client did not request any subprotocol
-              (lambda (c) (ws-send! c "You didn't explicitly request a subprotocol"))]]))
+              (lambda (c)
+
+                (ws-send! c "You didn't explicitly request a subprotocol")
+                (display "ws server no protocol msg received \n")
+
+              )]]))
 
 
 (require net/url)
-(define c (ws-connect (string->url "ws://localhost:8081/test")))
-(ws-send! c "Hello world!")
-(ws-send! c "Hello world!")
-(ws-send! c "Hello world!")
-(ws-send! c "Hello world!")
-(ws-send! c "Hello world!")
+(define client-a (ws-connect (string->url "ws://localhost:8081/path")))
+(define client-b (ws-connect (string->url "ws://localhost:8081/path")))
+(define client-c (ws-connect (string->url "ws://localhost:8081/path")))
+(ws-send! client-a "Hello world!")
+(ws-send! client-a "Hello world!")
+(ws-send! client-b "Hello world!")
+(ws-send! client-b "Hello world!")
